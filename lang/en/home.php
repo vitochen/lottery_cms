dc4e102955f3e69d4022565d64642352fdaad2f2
vitochen <vitochen15@gmail.com>
2022-07-20 15:36:48 +0800

    added cms introduce in home

---
+++ lang/en/home.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'welcome' => 'Welcome!',
+    'greeting' => ' This is a demo project about Lottery CMS. <br> Feel free to try our feature from sidebar.',
+
+    'event_intro_tile' => 'Event Introduce',
+    'member_intro_tile' => 'Member Introduce',
+];
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,36 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="alert alert-success alert-dismissable margin5">
+    <button type="button" class="btn-close float-end" data-bs-dismiss="alert" aria-label="Close"></button>
+    <strong>@lang('home.welcome')</strong>
+    @lang('home.greeting')
+
+</div>
+
+
+
 <div class="card">
     <div class="card-header">
-        <h1>Home</h1>
+        <button class="btn btn-link" data-bs-toggle="collapse" href="#collapseEvent" role="button" aria-expanded="false"
+            aria-controls="collapseEvent">
+            <h5>@lang('home.event_intro_tile')</h5>
+        </button>
     </div>
 
-    <div class="card-body">
-
-        <div class="table-responsive">
-            <table class="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                    </tr>
-                </tbody>
-            </table>
+    <div class="collapse show" id="collapseEvent">
+        <div class="card-body">
+            <img src="{{ \Illuminate\Support\Facades\Storage::url('public/event_intro.png') }}"
+                class="rounded img-fluid" alt="event_intro">
         </div>
+    </div>
+</div>
 
+<div class="card">
+    <div class="card-header">
+        <button class="btn btn-link" data-bs-toggle="collapse" href="#collapseMember" role="button" aria-expanded="false"
+            aria-controls="collapseMember">
+            <h5>@lang('home.member_intro_tile')</h5>
+        </button>
+    </div>
+
+    <div class="collapse" id="collapseMember">
+        <div class="card-body">
+            <img src="{{ \Illuminate\Support\Facades\Storage::url('public/member_intro.png') }}"
+                class="rounded img-fluid" alt="event_intro">
+        </div>
     </div>
 </div>
 
